cae86cd9806541574bd8fcebbdc0687cb1565096
Jiang Yin <jiangyin@gameframework.cn>
2016-12-15 23:42:23 +0800

    Upgrade GameFramework 3.0.0

---
+++ UnityGameFramework/Assets/GameFramework/Scripts/Editor/SceneComponentInspector.cs
@@ -0,0 +1,66 @@
+﻿//------------------------------------------------------------
+// Game Framework v3.x
+// Copyright © 2013-2017 Jiang Yin. All rights reserved.
+// Homepage: http://gameframework.cn/
+// Feedback: mailto:jiangyin@gameframework.cn
+//------------------------------------------------------------
+
+using UnityEditor;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace UnityGameFramework.Editor
+{
+    [CustomEditor(typeof(SceneComponent))]
+    internal sealed class SceneComponentInspector : GameFrameworkInspector
+    {
+        private SerializedProperty m_EnableLoadSceneSuccessEvent = null;
+        private SerializedProperty m_EnableLoadSceneFailureEvent = null;
+        private SerializedProperty m_EnableLoadSceneUpdateEvent = null;
+        private SerializedProperty m_EnableLoadSceneDependencyAssetEvent = null;
+        private SerializedProperty m_EnableUnloadSceneSuccessEvent = null;
+        private SerializedProperty m_EnableUnloadSceneFailureEvent = null;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+
+            SceneComponent t = target as SceneComponent;
+
+            EditorGUILayout.PropertyField(m_EnableLoadSceneSuccessEvent);
+            EditorGUILayout.PropertyField(m_EnableLoadSceneFailureEvent);
+            EditorGUILayout.PropertyField(m_EnableLoadSceneUpdateEvent);
+            EditorGUILayout.PropertyField(m_EnableLoadSceneDependencyAssetEvent);
+            EditorGUILayout.PropertyField(m_EnableUnloadSceneSuccessEvent);
+            EditorGUILayout.PropertyField(m_EnableUnloadSceneFailureEvent);
+
+            serializedObject.ApplyModifiedProperties();
+
+            if (EditorApplication.isPlaying)
+            {
+                EditorGUILayout.LabelField("Loaded Scene Names", GetSceneNameString(t.GetLoadedSceneNames()));
+                EditorGUILayout.LabelField("Loading Scene Names", GetSceneNameString(t.GetLoadingSceneNames()));
+                EditorGUILayout.ObjectField("Main Camera", t.MainCamera, typeof(Camera), true);
+
+                Repaint();
+            }
+        }
+
+        private void OnEnable()
+        {
+            m_EnableLoadSceneSuccessEvent = serializedObject.FindProperty("m_EnableLoadSceneSuccessEvent");
+            m_EnableLoadSceneFailureEvent = serializedObject.FindProperty("m_EnableLoadSceneFailureEvent");
+            m_EnableLoadSceneUpdateEvent = serializedObject.FindProperty("m_EnableLoadSceneUpdateEvent");
+            m_EnableLoadSceneDependencyAssetEvent = serializedObject.FindProperty("m_EnableLoadSceneDependencyAssetEvent");
+            m_EnableUnloadSceneSuccessEvent = serializedObject.FindProperty("m_EnableUnloadSceneSuccessEvent");
+            m_EnableUnloadSceneFailureEvent = serializedObject.FindProperty("m_EnableUnloadSceneFailureEvent");
+        }
+
+        private string GetSceneNameString(string[] sceneNames)
+        {
+            return sceneNames == null || sceneNames.Length <= 0 ? "<Empty>" : string.Join(", ", sceneNames);
+        }
+    }
+}
---
+++ UnityGameFramework/Assets/GameFramework/Scripts/Runtime/Resource/ResourceHelperBase.cs
@@ -0,0 +1,38 @@
+﻿//------------------------------------------------------------
+// Game Framework v3.x
+// Copyright © 2013-2017 Jiang Yin. All rights reserved.
+// Homepage: http://gameframework.cn/
+// Feedback: mailto:jiangyin@gameframework.cn
+//------------------------------------------------------------
+
+using GameFramework.Resource;
+using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 资源辅助器基类。
+    /// </summary>
+    public abstract class ResourceHelperBase : MonoBehaviour, IResourceHelper
+    {
+        /// <summary>
+        /// 直接从指定文件路径读取数据流。
+        /// </summary>
+        /// <param name="fileUri">文件路径。</param>
+        /// <param name="loadBytesCallback">读取数据流回调函数。</param>
+        public abstract void LoadBytes(string fileUri, LoadBytesCallback loadBytesCallback);
+
+        /// <summary>
+        /// 卸载场景。
+        /// </summary>
+        /// <param name="sceneName">场景名称。</param>
+        /// <returns>是否成功卸载场景。</returns>
+        public abstract bool UnloadScene(string sceneName);
+
+        /// <summary>
+        /// 释放资源。
+        /// </summary>
+        /// <param name="objectToRelease">要释放的资源。</param>
+        public abstract void Release(object objectToRelease);
+    }
+}
